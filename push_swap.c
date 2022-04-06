740bfe57a3dd18ceee8d709d47e477132199287c
Bodu Lyu <blyu@c1r29s9.42tokyo.jp>
2022-04-06 23:24:24 +0900

    ...

--- push_swap.c
+++ push_swap.c
@@ -3,7 +3,7 @@
 int push_swap(t_stack	*s, int ms)//ms: main_stack()
 {
 	t_stack	next;
-
+	size_t	mount;
 
 	if (ms <= _a && s->a_len <= 2)
 	{
@@ -12,8 +12,8 @@ int push_swap(t_stack	*s, int ms)//ms: main_stack()
 		return(0);
 	}
 	ft_bzero(&next, sizeof(t_stack));
-	xxx();/* 分ける基準を決める(それぞれどれだけの量の数があるかも求めたい←次でも可) */
-	xxx();/* ２つに分ける処理 */
+	mount = divide_mount();/* 分ける基準を決める(それぞれどれだけの量の数があるかも求めたい←次でも可) */
+	xxx(/*  */); /* ２つに分ける処理 */
 	if (ms != _a_first)
 		xxx();/* 底にあるものを上まで持ってくる処理 */
 	xxx();/* スタックを整える処理(_firstのために存在する) */
