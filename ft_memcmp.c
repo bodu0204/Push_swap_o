6277bd43909c117b6d675e85f97674f9046876da
Bodu Lyu <blyu@c1r29s1.42tokyo.jp>
2022-04-17 10:49:44 +0900

    ...

---
+++ ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eiino <eiino@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/07 07:59:25 by eiino             #+#    #+#             */
+/*   Updated: 2022/01/15 20:31:04 by eiino            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+int	ft_memcmp(const void	*s1, const void	*s2, size_t	n)
+{
+	const char	*str1;
+	const char	*str2;
+	size_t		i;
+
+	str1 = (const char *)s1;
+	str2 = (const char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
+		i++;
+	}
+	return (0);
+}
