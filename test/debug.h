3d4815546c529bada0f3de67bd66c579dda97099
呂明 <ryoakira@ryoakiranoMacBook-Pro.local>
2022-04-19 08:52:08 +0900

    commit form makefile

--- test/debug.h
+++ test/debug.h
@@ -2,7 +2,7 @@
 #define DEBUG_H
 #include <stdio.h>
 #define TEST printf("[%s/%d]\n", __FILE__, __LINE__); fflush(stdin);
-#define TESTn(str, i) printf("[%s/%d]%s:%d\n", __FILE__, __LINE__, (str), ((int)i)); fflush(stdin);
+#define TESTn(str, i) printf("[%s/%d]%s:%d\n", __FILE__, __LINE__, (str), (int)(i)); fflush(stdin);
 #define TESTp(str, i) printf("[%s/%d]%s:%p\n", __FILE__, __LINE__, (str), (i)); fflush(stdin);
 #endif
 //* test */static	size_t stc = 0;
