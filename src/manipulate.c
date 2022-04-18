f3971897b84209d345483d0013dfad05329a906a
呂明 <ryoakira@ryoakiranoMacBook-Pro.local>
2022-04-19 08:14:22 +0900

    commit form makefile

--- src/manipulate.c
+++ src/manipulate.c
@@ -7,7 +7,7 @@ int	manipulate4(t_stack	*s, int cmd);
 int	manipulate(t_stack	*s, int cmd)
 {
 	int	i;
-	if (cmd == sa)
+	if (cmd)
 	{
 		i = s->a[s->a_len - 1];
 		s->a[s->a_len - 1] = s->a[s->a_len - 2];
