e74b304b543d86d2902407ba73d6aec2577cb2fa
呂明 <ryoakira@ryoakiranoMacBook-Pro.local>
2022-04-20 08:06:37 +0900

    commit form makefile

--- src/debug.h
+++ src/debug.h
@@ -4,6 +4,9 @@
 #define TEST printf("[%s/%d]\n", __FILE__, __LINE__); fflush(stdin);
 #define TESTn(str, i) printf("[%s/%d]%s:%d\n", __FILE__, __LINE__, (str), (int)(i)); fflush(stdin);
 #define TESTp(str, i) printf("[%s/%d]%s:%p\n", __FILE__, __LINE__, (str), (i)); fflush(stdin);
+#ifndef PUSH_SWAP_H
+#include "push_swap.h"
+#endif
 void tests(t_stack *s);
 #endif
 //* test */static	size_t stc = 0;
