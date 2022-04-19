a99ae32398905d62db0b7bf350cc1e6f53804323
呂明 <ryoakira@ryoakiranoMacBook-Pro.local>
2022-04-20 08:45:36 +0900

    commit form makefile

--- src/push_swap.c
+++ src/push_swap.c
@@ -15,22 +15,22 @@ int	push_swap(t_stack	*s, int ms)
 /* test */}
 	t_dividing	d;
 
-printf("----------------------little_push_swap");TEST
+printf("----------------------little_push_swap");TESTn("s->a_len", s->a_len)
 	if (little_push_swap(s))
 		return (1);
 	if (!s->a_len && !s->b_len)
 		return (0);
-printf("----------------------set_divide_fmt");TEST
+printf("----------------------set_divide_fmt");TESTn("s->a_len", s->a_len)
 	set_divide_fmt(&d, s->g, s->g_len);
-printf("----------------------divide");TEST
+printf("----------------------divide");TESTn("s->a_len", s->a_len)
 	if (divide(s, &d, ms))
 		return (1);
-printf("----------------------treatstack");TEST
+printf("----------------------treatstack");TESTn("s->a_len", s->a_len)
 	if (treatstack(s, ms))
 		return (1);
 	if (s->a_len <= 2)
 	{
-printf("----------------------swaptwo");TEST
+printf("----------------------swaptwo");TESTn("s->a_len", s->a_len)
 		if (swaptwo(s, &d))
 			return (1);
 		if (!s->b_len)
@@ -43,15 +43,15 @@ int	push_swap1(t_stack	*s, t_dividing	*d)
 {
 	t_stack		next;
 
-printf("----------------------set_next_stack-a");TEST
+printf("----------------------set_next_stack-a");TESTn("s->a_len", s->a_len)
 	set_next_stack(s, &next, d, _a);
-printf("----------------------push_swap-a");TEST
+printf("----------------------push_swap-a");TESTn("s->a_len", s->a_len)
 	if (s->a_len)
 		if (push_swap(&next, _a))
 			return (1);
-printf("----------------------set_next_stack-b");TEST
+printf("----------------------set_next_stack-b");TESTn("s->a_len", s->a_len)
 	set_next_stack(s, &next, d, _b);
-printf("----------------------push_swap-b");TEST
+printf("----------------------push_swap-b");TESTn("s->a_len", s->a_len)
 	if (s->b_len)
 		if (push_swap(&next, _b))
 			return (1);
