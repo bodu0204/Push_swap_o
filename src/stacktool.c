ad40618ee3e1e23bf38b8fe9e29c08e61cc8a5d3
呂明 <ryoakira@ryoakiranoMacBook-Pro.local>
2022-04-22 17:17:32 +0900

    commit form makefile

--- src/stacktool.c
+++ src/stacktool.c
@@ -1,24 +1,19 @@
 #include "push_swap.h"
 #include "debug.h"/* test */
 
-void push(t_stack *s, int n)
-{
-	s->phs[(s->img + s->udr + s->grd + s->len) % s->phl] = n;
-	s->len++;
-if (s->udr + s->grd + s->len > s->phl)
-{printf("found error");TEST}
-	return ;
-}
-
-int pop(t_stack *s)
+void push(t_stack *o, t_stack *u)
 {
 	int i;
 
-if (!s->len)
+if (!o->len)
 {printf("found error");TEST}
-	s->len--;
-	i = s->phs[(s->img + s->udr + s->grd + s->len) % s->phl];
-	return (i);
+	o->len--;
+	i = o->phs[(o->img + o->udr + o->grd + o->len) % o->phl];
+	u->phs[(u->img + u->udr + u->grd + u->len) % u->phl] = i;
+	u->len++;
+if (u->udr + u->grd + u->len > u->phl)
+{printf("found error");TEST}
+	return ;
 }
 
 void swap(t_stack *s)
@@ -35,16 +30,40 @@ if (s->len < 2)
 	return ;
 }
 
-void rotate(t_stack *s)//未完
+void rotate(t_stack *s)
 {
 	int i;
-	int ii;
 
-if (s->len < 2)
+if (!s->len)
 {printf("found error");TEST}
-	i = s->phs[(s->img + s->udr + s->grd + s->len - 1) % s->phl];
-	ii = s->phs[(s->img + s->udr + s->grd + s->len - 2) % s->phl];
-	s->phs[(s->img + s->udr + s->grd + s->len - 1) % s->phl] = ii;
-	s->phs[(s->img + s->udr + s->grd + s->len - 2) % s->phl] = i;
+	s->len--;
+	i = s->phs[(s->img + s->udr + s->grd + s->len) % s->phl];
+	s->img = (s->img + s->phl - 1) % s->phl;
+	s->phs[s->img] = i;
+	s->udr++;
 	return ;
+}
+
+void revrotate(t_stack *s)
+{
+	int i;
+
+if (!s->udr)
+{printf("found error");TEST}
+	i = s->phs[s->img];
+	s->img = (s->img + 1) % s->phl;
+	s->udr--;
+	s->phs[(s->img + s->udr + s->grd + s->len) % s->phl] = i;
+	s->len++;
+	return ;
+}
+
+int	getn(t_stack *s, size_t l)
+{
+	int i;
+
+if (l >= s->len)
+{printf("found error");TEST}
+	i = s->phs[(s->img + s->udr + s->grd + l) % s->phl];
+	return (i);
 }
