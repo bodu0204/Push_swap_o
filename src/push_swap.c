66a95fbc5c4ea1fad2b6b4999dd1261feb2b9525
Bodu Lyu <blyu@c1r29s9.42tokyo.jp>
2022-04-24 09:18:26 +0900

    commit form makefile

--- src/push_swap.c
+++ src/push_swap.c
@@ -7,19 +7,13 @@ void	push_swap(t_situation	*s, t_mplh *h, int ms)
 
 	rase(s);
 	set_divide_fmt(&d, &s->g);
-	if (ms = _a && !s->a.grd && s->a.len == 5)
-	{
-		push_swap_five(s, h, &d);
+	if (push_swap_five(s, h, &d))
 		return ;
-	}
-	if ((ms = _a && s->a.len <= 4) || (ms = _b && s->b.len <= 4))
-	{
-		little_push_swap(s, h, &d, ms);
+	if (swap_thrtwo(s, h))
 		return ;
-	}
 	divide(s, h, &d, ms);
 	treatstack(s, h, ms);
-	if (swap_twotwo(s, h))
+	if (swap_thrtwo(s, h))
 		return ;
 	set_next_stack(s, &next, _a);
 	push_swap(&next, h, _a);
@@ -28,10 +22,12 @@ void	push_swap(t_situation	*s, t_mplh *h, int ms)
 	return ;
 }
 
-void	push_swap_five(t_situation	*s, t_mplh *h, t_dividing	*d)
+int	push_swap_five(t_situation	*s, t_mplh *h, t_dividing	*d)
 {
 	size_t	i;
 
+	if (s->a.grd || s->a.len != 5)
+		return (0);
 	while (getn(&s->a, 0, BOTTOM) < d->num)
 		manipulate(rra, s, h);
 	i = 0;
@@ -45,31 +41,45 @@ void	push_swap_five(t_situation	*s, t_mplh *h, t_dividing	*d)
 		else
 			manipulate(ra, s , h);
 	}
-	if (getn(&s->a, 1, DEAL) == getn(&s->g, 0, DEAL))
-		manipulate(rra, s , h);
-	else if(getn(&s->a, 2, DEAL) == getn(&s->g, 0, DEAL))
-		manipulate(ra, s , h);
-	s->a.grd++;
-	s->a.len--;
+	swap_thrtwo(s, h);
+	return (1);
 }
 
-int	swap_twotwo(t_situation	*s, t_mplh *h)
+int	swap_thrtwo(t_situation	*s, t_mplh *h)
 {
 	if (!s->a.len && !s->b.len)
 		return (1);
-	if ((s->a.len < 2 || (s->a.len == 2 && getn(s->a, 0, DEAL) >= getn(s->a, 1, DEAL))) && !s->b.len)
+	if (s->a.len == 3 && !s->a.grd && s->b.len <= 2)
 	{
-		s->a.grd += s->a.len;
-		s->a.len = 0;
-		return(1);
+		if (getn(&s->a, 1, DEAL) == getn(&s->g, 0, DEAL))
+		manipulate(rra, s , h);
+		else if(getn(&s->a, 2, DEAL) == getn(&s->g, 0, DEAL))
+			manipulate(ra, s , h);
+		s->a.grd++;
+		s->a.len--;
+		return (swap_thrtwo(s, h));
 	}
-	if (s->a.len < 2 && s->b.len <= 2)
+	if ((s->a.len < 2 || (s->a.len == 2 && getn(&s->a, 0, DEAL) >= getn(&s->a, 1, DEAL))) && s->b.len <= 2)
 	{
 		s->a.grd += s->a.len;
 		s->a.len = 0;
+		return(swap_thrtwo(s, h));
+	}
+	if (s->a.len == 2 && getn(&s->a, 0, DEAL) < getn(&s->a, 1, DEAL) && s->b.len <= 2)
+	{
+		manipulate(sa, s, h);
+		return(swap_thrtwo(s, h));
+	}
+	if (!s->a.len && (s->b.len < 2 || (s->b.len == 2 && getn(&s->b, 0, DEAL) <= getn(&s->b, 1, DEAL))))
+	{
 		while (s->b.len)
 			manipulate(pa, s, h);
-		
+		return (swap_thrtwo(s, h));
 	}
-	else
+	if (!s->a.len && (s->b.len < 2 || (s->b.len == 2 && getn(&s->b, 0, DEAL) > getn(&s->b, 1, DEAL))))
+	{
+		manipulate(sb, s, h);
+		return (swap_thrtwo(s, h));
+	}
+	return (0);
 }
