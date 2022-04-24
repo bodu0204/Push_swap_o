ae14393fae4e82ad693097029fb7b3e063edf94d
呂明 <ryoakira@ryoakiranoMacBook-Pro.local>
2022-04-24 20:06:27 +0900

    commit form makefile

--- src/stacktool.c
+++ src/stacktool.c
@@ -66,11 +66,11 @@ if (!s->udr && s->grd)
 
 int	getn(t_stack *s, size_t l, int flag)
 {
-//TESTn("l", l)
-//TESTn("flag", flag)
-//TESTn("s->len", s->len)
-//TESTn("s->grd", s->grd)
-//TESTn("s->udr", s->udr)
+TESTn("l", l)
+TESTn("flag", flag)
+TESTn("s->len", s->len)
+TESTn("s->grd", s->grd)
+TESTn("s->udr", s->udr)
 	if (flag == TOP)
 		l = s->img + s->udr + s->grd + s->len + s->phl - 1;
 	else if (flag == DEAL)
@@ -79,11 +79,11 @@ int	getn(t_stack *s, size_t l, int flag)
 		l += s->img + s->udr;
 	else if(flag == UNDER)
 		l += s->img;
-	else if(flag == BOTTOM)
+	else                                  if(flag == BOTTOM)
 		l = s->img;
 else
 {printf("found error");TEST exit(2);}
-//TESTn("l - s->img", l - s->img)
+TESTn("l - s->img - ((flag == TOP) * (s->phl - 1))", l - s->img - ((flag == TOP) * (s->phl - 1)))
 if (l - s->img - ((flag == TOP) * (s->phl - 1)) >= s->len + s->grd + s->udr)
 {printf("found error");TEST exit(2);}
 	return (s->phs[l % s->phl]);
