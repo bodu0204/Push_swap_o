47776beef3d38a0f217ef48fd678bbf25e3da5d5
呂明 <ryoakira@ryoakiranoMacBook-Pro.local>
2022-04-26 06:29:15 +0900

    commit form makefile

--- src/push_swap.c
+++ src/push_swap.c
@@ -59,49 +59,78 @@ int	push_swap_five(t_situation	*s, t_mplh *h, t_dividing	*d)
 	return (1);
 }
 
+//int	swap_thrtwo(t_situation	*s, t_mplh *h)
+//{
+////TEST_
+//	if (!s->a.len && !s->b.len)
+//{//TEST
+//		return (1);
+//}
+//	if (s->a.len == 3 && !s->a.grd && s->b.len <= 2)
+//	{
+////TEST
+//		if (getn(&s->a, 1, DEAL) == getn(&s->g, 0, DEAL))
+//		manipulate(rra, s , h);
+//		else if(getn(&s->a, 2, DEAL) == getn(&s->g, 0, DEAL))
+//			manipulate(ra, s , h);
+//		s->a.grd++;
+//		s->a.len--;
+//		return (swap_thrtwo(s, h));
+//	}
+//	if (s->a.len && (s->a.len < 2 || (s->a.len == 2 && getn(&s->a, 0, DEAL) >= getn(&s->a, 1, DEAL))) && s->b.len <= 2)
+//	{
+////TEST
+//		s->a.grd += s->a.len;
+//		s->a.len = 0;
+//		return(swap_thrtwo(s, h));
+//	}
+//	if (s->a.len == 2 && getn(&s->a, 0, DEAL) < getn(&s->a, 1, DEAL) && s->b.len <= 2)
+//	{
+////TEST
+//		manipulate(sa, s, h);
+//		return(swap_thrtwo(s, h));
+//	}
+//	if (!s->a.len && (s->b.len < 2 || (s->b.len == 2 && getn(&s->b, 0, DEAL) <= getn(&s->b, 1, DEAL))))
+//	{
+////TEST
+//		while (s->b.len)
+//			manipulate(pa, s, h);
+//		return (swap_thrtwo(s, h));
+//	}
+//	if (!s->a.len && (s->b.len < 2 || (s->b.len == 2 && getn(&s->b, 0, DEAL) > getn(&s->b, 1, DEAL))))
+//	{
+////TEST
+//		manipulate(sb, s, h);
+//		return (swap_thrtwo(s, h));
+//	}
+//	return (0);
+//}
+
 int	swap_thrtwo(t_situation	*s, t_mplh *h)
 {
-//TEST_
-	if (!s->a.len && !s->b.len)
-{//TEST
-		return (1);
-}
-	if (s->a.len == 3 && !s->a.grd && s->b.len <= 2)
+	raise(s);
+	if (!((s->a.len == 3 && !s->a.grd) || s->a.len <= 2 ) || s->b.len > 2 )
+		return (0);
+	if (s->a.len == 3)
 	{
-//TEST
 		if (getn(&s->a, 1, DEAL) == getn(&s->g, 0, DEAL))
 		manipulate(rra, s , h);
-		else if(getn(&s->a, 2, DEAL) == getn(&s->g, 0, DEAL))
+		else if (getn(&s->a, 2, DEAL) == getn(&s->g, 0, DEAL))
 			manipulate(ra, s , h);
-		s->a.grd++;
-		s->a.len--;
-		return (swap_thrtwo(s, h));
 	}
-	if (s->a.len && (s->a.len < 2 || (s->a.len == 2 && getn(&s->a, 0, DEAL) >= getn(&s->a, 1, DEAL))) && s->b.len <= 2)
+	raise(s);
+	if (s->a.len == 2)
 	{
-//TEST
-		s->a.grd += s->a.len;
-		s->a.len = 0;
-		return(swap_thrtwo(s, h));
+		if (s->b.len == 2 && getn(&s->b, 0, DEAL) > getn(&s->b, 1, DEAL))
+			manipulate(ss, s, h);
+		else
+			manipulate(sa, s, h);
 	}
-	if (s->a.len == 2 && getn(&s->a, 0, DEAL) < getn(&s->a, 1, DEAL) && s->b.len <= 2)
-	{
-//TEST
+	raise(s);
+	while (s->b.len)
+		manipulate(pa, s, h);
+	raise(s);
+	if (s->a.len == 2)
 		manipulate(sa, s, h);
-		return(swap_thrtwo(s, h));
-	}
-	if (!s->a.len && (s->b.len < 2 || (s->b.len == 2 && getn(&s->b, 0, DEAL) <= getn(&s->b, 1, DEAL))))
-	{
-//TEST
-		while (s->b.len)
-			manipulate(pa, s, h);
-		return (swap_thrtwo(s, h));
-	}
-	if (!s->a.len && (s->b.len < 2 || (s->b.len == 2 && getn(&s->b, 0, DEAL) > getn(&s->b, 1, DEAL))))
-	{
-//TEST
-		manipulate(sb, s, h);
-		return (swap_thrtwo(s, h));
-	}
-	return (0);
+	return (1);
 }
